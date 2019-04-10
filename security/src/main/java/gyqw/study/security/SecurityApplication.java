650ca3d3842ed06c2a19aec8b575226ff1f7a164
guye <guye@geexfinance.com>
2019-04-10 15:35:55 +0800

    添加security模块

---
+++ security/src/main/java/gyqw/study/security/SecurityApplication.java
@@ -0,0 +1,16 @@
+package gyqw.study.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author fred
+ * 2019-04-10 3:06 PM
+ */
+@ComponentScan
+public class SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class);
+    }
+}
