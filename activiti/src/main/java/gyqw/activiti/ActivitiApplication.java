bde86dafbdb34c4633760f48f04f0bbb86506986
guye <guye@geexfinance.com>
2019-04-25 15:21:40 +0800

    lambda test

---
+++ activiti/src/main/java/gyqw/activiti/ActivitiApplication.java
@@ -0,0 +1,11 @@
+package gyqw.activiti;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author fred
+ * 2019-04-25 2:16 PM
+ */
+@SpringBootApplication
+public class ActivitiApplication {
+}
---
+++ activiti/src/main/java/gyqw/activiti/lambda/LambdaTest.java
@@ -0,0 +1,41 @@
+package gyqw.activiti.lambda;
+
+import org.activiti.api.process.runtime.connector.Connector;
+import org.activiti.api.runtime.model.impl.IntegrationContextImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author fred
+ * 2019-04-25 2:15 PM
+ */
+public class LambdaTest {
+    private Logger logger = LoggerFactory.getLogger(LambdaTest.class);
+
+    public static void main(String[] args) {
+        LambdaTest lambdaTest = new LambdaTest();
+
+        IntegrationContextImpl integrationContext = new IntegrationContextImpl();
+        Content content = new Content("body", false, null);
+        Map<String, Object> map = new HashMap<>();
+        map.put("content", content);
+        integrationContext.setInBoundVariables(map);
+
+        lambdaTest.connector().apply(integrationContext);
+    }
+
+    private Connector connector() {
+        return integrationContext -> {
+            logger.info(integrationContext.toString());
+            Content contentToDiscard = (Content) integrationContext.getInBoundVariables().get("content");
+            contentToDiscard.getTags().add(" :( ");
+            integrationContext.addOutBoundVariable("content",
+                    contentToDiscard);
+            logger.info("Final Content: " + contentToDiscard);
+            return integrationContext;
+        };
+    }
+}
