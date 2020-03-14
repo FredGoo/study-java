7f93f1d1fd14de27c6d72828e144e38fe4b9fcf7
Fred Goo <fredera92@gmail.com>
2020-03-14 20:38:41 +0800

    <clinit>()阻塞模拟

--- cs/src/main/java/gyqw/test/ClassLoaderTest.java
+++ cs/src/main/java/gyqw/test/ClassLoaderTest.java
@@ -1,5 +1,6 @@
 package gyqw.test;
 
+import gyqw.model.classloader.DeadLoopModel;
 import gyqw.util.Logger;
 import org.junit.jupiter.api.Test;
 
@@ -11,9 +12,23 @@ public class ClassLoaderTest {
     private Logger logger = new Logger();
 
     @Test
-    public void parentLoader() {
+    public void parentLoaderTest() {
         logger.info("ClassLoaderTest's ClassLoader is: %s", ClassLoaderTest.class.getClassLoader());
         logger.info("The Parent of ClassLoaderTest's ClassLoader is: %s", ClassLoaderTest.class.getClassLoader().getParent());
         logger.info("The GrandParent of ClassLoaderTest's ClassLoader is: %s", ClassLoaderTest.class.getClassLoader().getParent().getParent());
+    }
+
+    @Test
+    public void deadLoopTest() {
+        Runnable script = () -> {
+            logger.info("%s start", Thread.currentThread());
+            DeadLoopModel dlm = new DeadLoopModel();
+            logger.info("%s run over", Thread.currentThread());
+        };
+
+        Thread thread1 = new Thread(script);
+        Thread thread2 = new Thread(script);
+        thread1.start();
+        thread2.start();
     }
 }
