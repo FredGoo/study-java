2d208c04648e133b6a59919fb9094f3dbb920e57
Fred Goo <fredera92@gmail.com>
2020-11-16 14:46:35 +0800

    predict

--- jpmml/src/main/java/gyqw/pmml/PMMLPrediction.java
+++ jpmml/src/main/java/gyqw/pmml/PMMLPrediction.java
@@ -1,88 +1,87 @@
 package gyqw.pmml;
 
+import org.apache.http.HttpHost;
 import org.dmg.pmml.FieldName;
-import org.dmg.pmml.Model;
-import org.dmg.pmml.PMML;
-import org.dmg.pmml.mining.MiningModel;
-import org.dmg.pmml.scorecard.Scorecard;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.jpmml.evaluator.*;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.File;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PMMLPrediction {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws Exception {
+        String appId = "NYB01-201113-440553";
         PMMLPrediction demo = new PMMLPrediction();
-        Evaluator model = demo.loadPmml();
+        Evaluator evaluator = demo.initModel();
+        demo.predict(evaluator,
+                demo.getMapping(),
+                demo.getDataByAppId(appId));
     }
 
-    private Evaluator loadPmml() {
-        PMML pmml = new PMML();
-        InputStream inputStream = null;
-        try {
-            inputStream = new FileInputStream("/home/fred/git/study-java/jpmml/src/main/resources/lightgbm.pmml");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        if (inputStream == null) {
-            return null;
-        }
-        InputStream is = inputStream;
-        try {
-            pmml = org.jpmml.model.PMMLUtil.unmarshal(is);
-        } catch (Exception e1) {
-            e1.printStackTrace();
-        } finally {
-            //关闭输入流
-            try {
-                is.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        ModelEvaluatorFactory modelEvaluatorFactory = ModelEvaluatorFactory.newInstance();
-        Model model = new MiningModel();
-        Evaluator evaluator = modelEvaluatorFactory.newModelEvaluator(pmml, model);
-
+    private Evaluator initModel() throws Exception {
+        String filePath = "/home/fred/git/study-java/jpmml/src/main/resources/CapitalDecisionTree.pmml";
+        File file = new File(filePath);
+        Evaluator evaluator = new LoadingModelEvaluatorBuilder()
+                .load(file)
+                .build();
+        evaluator.verify();
         return evaluator;
     }
 
-    private int predict(Evaluator evaluator, int a, int b, int c, int d) {
-        Map<String, Integer> data = new HashMap<>();
-        data.put("x1", a);
-        data.put("x2", b);
-        data.put("x3", c);
-        data.put("x4", d);
-        List<InputField> inputFields = evaluator.getInputFields();
-        // 过模型的原始特征，从画像中获取数据，作为模型输入
+    private void predict(Evaluator evaluator, Map<String, String> mapping, Map<String, Object> data) {
+        // 获取模型定义的特征
+        List<? extends InputField> inputFields = evaluator.getInputFields();
+        System.out.println("模型的特征是：" + inputFields);
+        // 获取模型定义的目标名称
+        List<? extends TargetField> targetFields = evaluator.getTargetFields();
+        System.out.println("目标字段是：" + targetFields);
+
+        // 好，下面将json转成evaluator要求的map格式，其实就是对key和value再做一层包装而已
         Map<FieldName, FieldValue> arguments = new LinkedHashMap<>();
         for (InputField inputField : inputFields) {
-            FieldName inputFieldName = inputField.getName();
-            Object rawValue = data.get(inputFieldName.getValue());
-            FieldValue inputFieldValue = inputField.prepare(rawValue);
-            arguments.put(inputFieldName, inputFieldValue);
+            FieldName inputName = inputField.getName();
+            String name = mapping.get(inputName.getValue());
+            Object rawValue = data.get(name);
+            FieldValue inputValue = inputField.prepare(rawValue);
+            arguments.put(inputName, inputValue);
         }
 
+        // 得到特征数据后就是预测了
         Map<FieldName, ?> results = evaluator.evaluate(arguments);
-        List<TargetField> targetFields = evaluator.getTargetFields();
+        Map<String, ?> resultRecord = (Map<String, ?>) EvaluatorUtil.decode(results);
+        Integer y = (Integer) resultRecord.get("y");
 
-        TargetField targetField = targetFields.get(0);
-        FieldName targetFieldName = targetField.getName();
+        // 打印结果会更加了解其中的封装过程
+        System.out.println("预测结果：");
+        System.out.println(results);
+        System.out.println(resultRecord);
+        System.out.println(y);
+    }
 
-        Object targetFieldValue = results.get(targetFieldName);
-        System.out.println("target: " + targetFieldName.getValue() + " value: " + targetFieldValue);
-        int primitiveValue = -1;
-        if (targetFieldValue instanceof Computable) {
-            Computable computable = (Computable) targetFieldValue;
-            primitiveValue = (Integer) computable.getResult();
+    private Map<String, String> getMapping() {
+        String[] fieldArray = FieldMapModel.fields.split(",");
+        Map<String, String> mapping = new HashMap<>();
+        for (int i = 0; i < fieldArray.length; i++) {
+            mapping.put("x" + (i + 1), fieldArray[i]);
         }
-        System.out.println(a + " " + b + " " + c + " " + d + ":" + primitiveValue);
-        return primitiveValue;
+        return mapping;
+    }
+
+    private Map<String, Object> getDataByAppId(String appId) throws Exception {
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http"))
+        );
+        GetRequest getRequest = new GetRequest("app-info-index", appId);
+        GetResponse getResponse = client.get(getRequest, RequestOptions.DEFAULT);
+        client.close();
+        return getResponse.getSourceAsMap();
     }
 }
